c99615dc8f93f8a248c5ccd0bb335e32174ba7d9
hogumachu <castle_jun@daum.net>
2023-02-22 16:51:57 +0900

    init

---
+++ Selfer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Selfer
+//
+//  Created by 홍성준 on 2023/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
